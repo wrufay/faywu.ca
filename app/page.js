6d5ca5c9ce7b2da689f6c883463d94be1922e64a
fay wu <wru.fay@gmail.com>
2025-12-11 19:28:26 -0500

    buttons fix

--- app/page.js
+++ app/page.js
@@ -9,14 +9,14 @@ export default function Home() {
           alt=""
         /> */}
         <div className="max-w-2xl text-center">
-          <h1 className="text-4xl md:text-5xl font-bold mb-6 typing-animation">
+          <h1 className="text-4xl md:text-6xl font-bold mb-10 typing-animation">
             hi there, i'm fay!
           </h1>
           <p className="text-l md:text-2xl text-[#da6319] mb-8 fade-in delay-1">
             studying cs @ uwaterloo (1a)
           </p>
           {/*buttons*/}
-          <div className="flex gap-4 justify-center fade-in-bounce-delayed">
+          <div className="flex flex-col sm:flex-row gap-4 justify-center items-center fade-in-bounce-delayed">
             <a
               href="https://www.linkedin.com/in/fayranw/"
               className="px-2 py-1 md:px-4 md:py-2 text-black border border-black rounded-lg hover:bg-white transition"

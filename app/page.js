6eb02401d66449bc1eb40ff68e8abfd0420bcd05
fay wu <wru.fay@gmail.com>
2025-12-25 00:05:43 -0400

    fix on small screens

--- app/page.js
+++ app/page.js
@@ -5,18 +5,18 @@ export default function Home() {
       <section className="flex flex-col items-center min-h-screen px-4 justify-center">
         <img
           src="/me-best.png"
-          className="w-48 h-48 sm:w-64 sm:h-64 object-cover mx-auto mb-4 fade-in"
+          className="w-48 h-48 sm:w-64 sm:h-64 object-cover mx-auto mb-2 sm:mb-4 fade-in"
           alt=""
         />
         <div className="max-w-2xl text-center">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl font-black text-black mb-10 typing-animation">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-black text-black mb-8 sm:mb-10 typing-animation">
             hi there, i'm fay!
           </h1>
-          <p className="text-l sm:text-2xl text-[var(--red-middle)] mb-8 fade-in delay-1 font-bold">
+          <p className="text-l sm:text-2xl text-[var(--red-middle)] mb-4 sm:mb-8 fade-in delay-1 font-bold">
             an aspiring software developer who loves design.
           </p>
           {/*buttons*/}
-          <div className="flex flex-col sm:flex-row gap-4 justify-center items-center fade-in-bounce-delayed">
+          <div className="flex flex-col sm:flex-row gap-2 sm:gap-4 justify-center items-center fade-in-bounce-delayed text-sm sm:text-base">
             <a
               href="https://www.linkedin.com/in/fayranw/"
               target="_blank"

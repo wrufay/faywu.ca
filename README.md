78fb88388ec497b02f56dfaa06ba863b656bbc17
fay wu <wru.fay@gmail.com>
2026-01-01 00:18:01 -0500

    edit somehting idk

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 todo:
-possibly replace footer text w/ logos (handdrawn)
-incorporate more handrawn stuff
 actually make the collections work
--- app/layout.js
+++ app/layout.js
@@ -11,12 +11,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       {/* need to make a colour scheme :) */}
       {/* bg-linear-65 from-[#ffe3e0] to-[#ffcf99]  */}
-      <body className="bg-amber-50/30 text-black serif-regular text-center min-h-screen items-center flex flex-col px-8">
+      <body className="bg-amber-50/30 text-gray-700 serif-regular text-center min-h-screen items-center flex flex-col px-8">
         {/* mini nav */}
-        <nav className="mt-8 sm:mt-18 flex justify-center gap-6">
+        <nav className="mt-8 sm:mt-18 flex justify-center items-center gap-6">
           <Link href="/" className="hover:opacity-67">
             ↻
           </Link>
+
           {/* <Link href="/about" className=" hover:opacity-67 font-black">
             about me
           </Link> */}

b9c357e9dcd5eb20fae4cc523e7fb9a35f5a0ac1
fay wu <wru.fay@gmail.com>
2026-01-06 13:48:01 -0500

    update gallery page

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -10,44 +10,36 @@ export default function Test() {
       url: "/paintings/colby.png",
       year: "2021",
       medium: "oil pastel on watercolour paper",
-      description: "rain somehow feels sweeter when you're home",
     },
     {
       title: "friend's doggos",
       url: "/paintings/dogs.jpg",
       year: "2022",
       medium: "acrylic on canvas",
-      description: "maybe one day animals can live forever",
     },
     {
-      title: "busses in japan",
+      title: "a bus somewhere in japan",
       url: "/paintings/bus.png",
       year: "2022",
       medium: "acrylic on paper",
-      description:
-        "let's hop on a bus that goes nowhere, yet everywhere at the same time.",
     },
     {
       title: "a house in a peaceful neighbourhood",
       url: "/paintings/house.png",
       year: "2022",
       medium: "acrylic on paper",
-      description:
-        "we really are teeny tiny, just like this painting, aren't we?",
     },
     {
       title: "highway somewhere",
       url: "/paintings/cars.png",
       year: "2022",
       medium: "oil pastel on paper",
-      description: "the sky isn't always bluer on the other side.",
     },
     {
-      title: "sketch of my cats",
+      title: "my cats",
       url: "/paintings/cats.png",
       year: "2022",
       medium: "oil pastel on paper",
-      description: "a couple strokes here, a couple strokes there...",
     },
 
     {
@@ -55,62 +47,50 @@ export default function Test() {
       url: "/paintings/garden.png",
       year: "2023",
       medium: "charcoal on paper",
-      description: "sunny days won't last forever, you know.",
     },
     {
       title: "sketch from life",
       url: "/paintings/hand.png",
       year: "2023",
       medium: "acrylic on mixed media sketchbook",
-      description:
-        "dear left hand, you don't do much, but i'll forever love you as my reference.",
     },
 
     {
-      title: "art 10 unit project",
+      title: "art 10 final project",
       url: "/paintings/abstract.png",
       year: "2023",
       medium: "mixed media on watercolour paper",
-      description: "the world is more vast than you can fathom.",
     },
     {
-      title: "still life, art 10",
+      title: "still life",
       url: "/paintings/stilllife.png",
       year: "2023",
       medium: "acrylic on paper",
-      description: "it's about finding beauty in the mundane.",
     },
     {
       title: "wolfville, nova scotia",
       url: "/paintings/valley.png",
       year: "2023",
       medium: "acrylic on paper",
-      description:
-        "wind blows, water flows. love grows, and so do the things we'll never know.",
     },
     {
       title: "halifax, nova scotia",
       url: "/paintings/citadel.png",
       year: "2023",
       medium: "acrylic on paper",
-      description:
-        "citadel hill: a place of peace and perhaps longing for change.",
     },
 
     {
-      title: "for my grandma",
+      title: "painting for my grandma",
       url: "/paintings/grandma.png",
       year: "2024",
       medium: "acrylic on paper",
-      description:
-        "the sad part is that time's a thief, and she'll never be caught.",
     },
     {
-      title: "window view",
+      title: "japan window view",
       url: "/paintings/japan.jpg",
       year: "2024",
       medium: "acrylic on muji sketchbook",
-      description: "shout out to the roads we'll never walk.",
     },
   ];
   const [curPainting, setCurPainting] = useState(0);
@@ -123,13 +103,16 @@ export default function Test() {
     setCurPainting((cur) => (cur <= 0 ? maxIndex : cur - 1));
   };
   return (
-    <main className="py-10 flex flex-grow justify-center items-center">
+    <main className="py-10 flex flex-grow flex-col gap-4 sm:gap-6 justify-center items-center">
+      <p className="text-sm">favourite pieces throughout the years</p>
       {/* painting card */}
       <div className="max-w-sm flex flex-col gap-2 sm:gap-4 bg-white shadow-sm px-8 py-6 sm:px-10 sm:py-8 border border-gray-100 rounded-sm fade-in">
         <p className="text-[9px] sm:text-xs text-gray-500 flex flex-row justify-between coding-regular">
           {/* index+1 and title  */}
           <span className="text-left">{paintings[curPainting].title}</span>
-          <span className="text-right">{curPainting + 1}</span>{" "}
+          <span className="text-right">
+            {curPainting + 1}/{maxIndex + 1}
+          </span>{" "}
         </p>
         <div className="flex flex-row justify-center items-center gap-3 sm:gap-4">
           {/* prev button */}
@@ -141,7 +124,7 @@ export default function Test() {
 
           <Image
             key={curPainting}
-            className="w-full border border-gray-200 opacity-86 rounded-sm select-none"
+            className="w-full border border-gray-200 opacity-86 rounded-sm select-none "
             src={paintings[curPainting].url}
             alt={paintings[curPainting].title}
             width={400}
@@ -156,18 +139,12 @@ export default function Test() {
           />
         </div>
         {/* item description */}
-        <div className="flex flex-col gap-4 sm:gap-6">
-          <p className="text-[9px] sm:text-xs text-gray-500 flex flex-row justify-between coding-regular">
-            {/* year and medium  */}
-            <span className="text-left">
-              {paintings[curPainting].year}
-            </span>{" "}
-            <span className="text-right">{paintings[curPainting].medium}</span>
-          </p>
-          <p className="text-xs sm:text-sm">
-            {paintings[curPainting].description}
-          </p>
-        </div>
+
+        <p className="text-[9px] sm:text-xs text-gray-500 flex flex-row justify-between coding-regular">
+          {/* year and medium  */}
+          <span className="text-left">{paintings[curPainting].year}</span>{" "}
+          <span className="text-right">{paintings[curPainting].medium}</span>
+        </p>
       </div>
 
       {/* Hidden preload images */}

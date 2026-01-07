a4e75de7c51b6a81116c5b6dcedaee493ee8f362
fay wu <wru.fay@gmail.com>
2026-01-07 09:20:28 -0500

    add collections page updated

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -3,8 +3,16 @@ import CollectionsItem from "@/components/CollectionsItem";
 export default function Collections() {
   return (
     <main className="py-10 flex flex-grow flex-col gap-4 sm:gap-6 justify-center items-center">
-      <CollectionsItem name="blog"></CollectionsItem>
-      <CollectionsItem name="gallery"></CollectionsItem>
+      <CollectionsItem
+        name="blog"
+        description="words and thoughts, sometimes coherently put together"
+        img="/projectpics/blog.png"
+      ></CollectionsItem>
+      <CollectionsItem
+        name="gallery"
+        description="some of my favourite art pieces throughout the years"
+        img="/projectpics/gallery.png"
+      ></CollectionsItem>
     </main>
   );
 }
--- components/CollectionsItem.tsx
+++ components/CollectionsItem.tsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 interface CollectionsItemProps {
   name: string;
+  description: string;
+  img: string;
 }
 
 export default function CollectionsItem(props: CollectionsItemProps) {
   return (
     <>
-      <div className="flex flex-col hover:translate-y-[-4px] w-xs transition-transform shadow-sm hover:shadow-md serif-regular bg-white w-full max-w-xs md:w-xs rounded-lg opacity-0 fade-in border border-gray-100">
+      <div className="flex flex-col hover:translate-y-[-4px] w-full max-w-xs sm:w-xs transition-transform shadow-sm hover:shadow-md serif-regular bg-white w-full max-w-xs md:w-xs rounded-lg opacity-0 fade-in border border-gray-100">
         <img
-          src="{image}"
+          src={props.img}
           className="h-32 w-full object-cover opacity-86 rounded-t-lg"
         />
 
@@ -21,14 +23,15 @@ export default function CollectionsItem(props: CollectionsItemProps) {
           {/* emoji and title */}
           <div className="flex flex-row justify-between items-center">
             <span className="flex items-center gap-2">
-              <img src="{logoLink}" className="w-5 h-5" />
+              <img src="icons/star.png" className="w-5 h-5 " />
               <h3 className="text-lg text-gray-800">{props.name}</h3>
             </span>
-            <p className="text-xs coding-regular text-gray-300">hdas</p>
+            {/* date thing idk what to put here yet */}
+            {/* <p className="text-xs coding-regular text-gray-300">hdas</p> */}
           </div>
 
           {/* description of project */}
-          <p className="text-sm text-left text-gray-600">hi</p>
+          <p className="text-sm text-left text-gray-600">{props.description}</p>
         </Link>
       </div>
     </>

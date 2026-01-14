1a206e1cadb9d27f4733daca35f72b25affcd36d
fay wu <wru.fay@gmail.com>
2026-01-14 00:42:50 -0500

    add additional button

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -4,15 +4,7 @@ import Link from "next/link";
 export default function Collections() {
   return (
     <main className="py-10 flex flex-grow justify-center flex-col gap-10 items-center">
-      <Link
-            href="/message"
-            className="px-3 py-2 sm:px-4 bg-white sm:py-2 hover:translate-y-[-2px] transition-transform border border-gray-300 text-gray-800 rounded-lg hover:bg-[var(--sunny-yellow)]/15"
-          >
-            send a{" "}
-            <span className="text-[var(--aritzia-blue)] coding-bold">
-              message
-            </span>
-          </Link>
+      
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
       
         {/* only gonna be like 2 items so not gomnna put an array */}
@@ -32,7 +24,17 @@ export default function Collections() {
           keyword="artwork"
           delay={300}
         ></CollectionsItem>
+       
       </div>
+      <Link
+            href="/message"
+            className="px-3 py-2 sm:px-4 sm:py-2 max-w-xs sm:w-xs hover:translate-y-[-2px] transition-transform border border-gray-300 text-gray-800 rounded-lg bg-[var(--sunny-yellow)]/15"
+          >
+            have a{" "}
+            <span className="text-[var(--aritzia-blue)] coding-bold">
+              suggestion
+            </span> for my site?
+          </Link>
     </main>
   );
 }

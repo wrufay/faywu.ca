4c47613524a3034efb2b2264cf7ebe6821437ed7
wrufay <wru.fay@gmail.com>
2026-05-27 10:59:50 -0300

    update front text

--- app/page.tsx
+++ app/page.tsx
@@ -35,13 +35,13 @@ export default function Home() {
           <p className="flex flex-row gap-1 sm:gap-2 items-center">
             <img src="/icons/star.png" className="w-3.5 h-3.5 sm:w-5 sm:h-5" />
             <span>
-              swe @{" "}
+              research assistant @{" "}
               <a
                 className="hover:text-[var(--crimson-red)] hover:opacity-100"
                 target="_blank"
                 href="https://www.dfo-mpo.gc.ca/index-eng.html"
               >
-                <span className="pen-regular">Fisheries & Oceans Canada</span>
+                <span className="pen-regular">DFO</span>
               </a>{" "}
               🐠
             </span>

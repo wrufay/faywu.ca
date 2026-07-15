5f4f21f5a0cbe0873f330482fc8fe16a37c1db1f
fay wu <wru.fay@gmail.com>
2026-07-15 16:12:49 -0300

    chore: edit resume link - final. and add back x + email in footer

--- components/FooterLinks.tsx
+++ components/FooterLinks.tsx
@@ -5,6 +5,12 @@ export default function FooterLinks() {
   return (
     <div className="flex gap-1 sm:gap-2 items-center">
       <a
+        href="mailto:f26wu@uwaterloo.ca"
+        className="opacity-50 hover:opacity-67 w-4 h-4 sm:w-5 sm:h-5 hover:translate-y-[-2px]"
+      >
+        <img src="/icons/email.png" />
+      </a>
+      <a
         href="https://www.linkedin.com/in/fayranw/"
         target="_blank"
         className="opacity-50 hover:opacity-67 w-4 h-4 sm:w-5 sm:h-5 hover:translate-y-[-2px]"
@@ -20,6 +26,13 @@ export default function FooterLinks() {
         className="opacity-50 hover:opacity-67 w-4 h-4 sm:w-5 sm:h-5 hover:translate-y-[-2px]"
       >
         <img src="/icons/github.png" />
+      </a>
+      <a
+        href="https://x.com/wrufay"
+        target="_blank"
+        className="opacity-50 hover:opacity-67 w-4 h-4 sm:w-5 sm:h-5 hover:translate-y-[-2px]"
+      >
+        <img src="/icons/x.png" />
       </a>
     </div>
   );
